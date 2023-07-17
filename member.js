00ff5d93a1c68385a493654d08ceb8991480a7d5
brancoisrael <brancoisrael@gmail.com>
2023-07-17 18:31:36 -0300

    Copilot second commit

--- member.js
+++ member.js
@@ -1,4 +1,4 @@
-function skillMember() {
+function skillsMember() {
   var member = document.getElementById("member");
   var skill = document.getElementById("skill");
   var memberText = document.getElementById("memberText");
