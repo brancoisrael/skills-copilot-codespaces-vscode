13410543ab1f505fef9ad0a8407f5fb684835172
brancoisrael <brancoisrael@gmail.com>
2023-07-17 18:26:02 -0300

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillMember() {
+  var member = document.getElementById("member");
+  var skill = document.getElementById("skill");
+  var memberText = document.getElementById("memberText");
+  var skillText = document.getElementById("skillText");
+  var memberImg = document.getElementById("memberImg");
+  var skillImg = document.getElementById("skillImg");
+  member.style.backgroundColor = "#fff";
+  skill.style.backgroundColor = "#fff";
+  memberText.style.color = "#000";
+  skillText.style.color = "#000";
+  memberImg.style.display = "block";
+  skillImg.style.display = "none";
+}
